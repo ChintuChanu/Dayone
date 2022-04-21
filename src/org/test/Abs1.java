f95bed8d5ff05f443bb112266f1b555628d58385
chintuchanu <Chintuchanu10@gmail.com>
2022-04-21 09:43:54 +0530

    new code added for absratcion

---
+++ src/org/test/Abs1.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public abstract class Abs1 {
+	public void empName() {
+		System.out.println("Ram");
+	}
+	public abstract void empId();
+
+}
---
+++ src/org/test/Abs2.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Abs2 extends Abs1 {
+
+	public void empId() {
+		System.out.println("746");
+		
+	}
+	public static void main(String[] args) {
+		Abs2 a = new Abs2();
+		a.empId();
+		a.empName();
+	}
+	
+
+}
